432f5be81a51cec39e79e6aa1b5dad3c5577e319
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-11-26 06:49:59 +0200

    finished basic spring security

--- src/main/java/com/spring_security/spring_security/Entity/User.java
+++ src/main/java/com/spring_security/spring_security/Entity/User.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users_table")
 public class User {
 
       
--- src/main/java/com/spring_security/spring_security/Service/UserService.java
+++ src/main/java/com/spring_security/spring_security/Service/UserService.java
@@ -1,23 +1,95 @@
 package com.spring_security.spring_security.Service;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.spring_security.spring_security.Entity.User;
 import com.spring_security.spring_security.Repository.UserRepository;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.var;
+
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private String encryptPassword(String password) {
+        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode(password);
+    }
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
 
     }
 
-    public void UserRegisterService(User user) {
+    public String UserRegisterService(User user) {
+
+        if (user == null || user.getEmail() == null || user.getPassword() == null || user.getUsername() == null) {
+            throw new IllegalArgumentException("Incomplete fields, PLease  provide all fields");
+        }
+
+        var userExists = userRepository.findByEmail(user.getEmail());
+
+        if (userExists != null) {
+            throw new IllegalArgumentException("user Already Exists");
+        }
+
+        String userEncryptedPassword = encryptPassword(user.getPassword());
+        user.setPassword(userEncryptedPassword);
+        
+        user.setPassword(userEncryptedPassword);
+
+        userRepository.save(user);
+
+        return " you are have  successfully  registered";
+
+    }
+
+    public String userLoginService(String userEmail, String userPassword) {
+
+        if (userEmail == null || userPassword == null) {
+            throw new IllegalArgumentException("pleae  provide all fields");
+        }
+
+        User UserExists = userRepository.findByEmail(userEmail);
+        
+
+        if (UserExists == null) {
+            throw new IllegalArgumentException("User not found.");
+        }
+        if (!passwordEncoder.matches(userPassword, UserExists.getPassword())) {
+            throw new IllegalArgumentException("Invalid email or password");
+
+        }
+      return generateToken(UserExists);
+
+    }
+
+
+
+    @SuppressWarnings("deprecation")
+    public String generateToken(User user){
+
+          String secreteKey= "My_secrete";
+          long expiredDuration= 360000l;
+
+          return Jwts.builder()
+          .setSubject(user.getEmail())
+          .claim("userName", user.getUsername()) 
+          .claim("roles", "USER") 
+          .setIssuedAt(new Date())
+          .setExpiration(new Date(System.currentTimeMillis() + expiredDuration))
+          .signWith(SignatureAlgorithm.HS512, secreteKey)
+          .compact();
+ 
 
     }
 
